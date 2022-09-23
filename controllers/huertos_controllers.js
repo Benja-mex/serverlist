750d00d5f14ff45acf9355111b270089a8318108
benja <benja.normex@gmail.com>
2022-09-23 17:24:26 -0500

    first commit

---
+++ controllers/huertos_controllers.js
@@ -0,0 +1,298 @@
+const { response, request } = require('express');
+
+const acuitzioGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Acuitzio.json');
+
+    res.json(jsonData);
+}
+const angangueoGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Angangueo.json');
+
+    res.json(jsonData);
+}
+const apatzinganGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Apatzingan.json');
+
+    res.json(jsonData);
+}
+const arioGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Ario.json');
+
+    res.json(jsonData);
+}
+const charapanGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Charapan.json');
+
+    res.json(jsonData);
+}
+const charoGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Charo.json');
+
+    res.json(jsonData);
+}
+const churumucoGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Churumuco.json');
+
+    res.json(jsonData);
+}
+const cotijaGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Cotija.json');
+
+    res.json(jsonData);
+}
+const erongaricuaroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Erongaricuaro.json');
+
+    res.json(jsonData);
+}
+const grabrielzamoraGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/GabrielZamora.json');
+
+    res.json(jsonData);
+}
+const hidalgoGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Hidalgo.json');
+
+    res.json(jsonData);
+}
+const huirambaGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Huiramba.json');
+
+    res.json(jsonData);
+}
+const irimboGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Irimbo.json');
+
+    res.json(jsonData);
+}
+const jimenezGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Jimenez.json');
+
+    res.json(jsonData);
+}
+const losreyesGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/LosReyes.json');
+
+    res.json(jsonData);
+}
+const maderoGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Madero.json');
+
+    res.json(jsonData);
+}
+const moreliaGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Morelia.json');
+
+    res.json(jsonData);
+}
+const morelosGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Morelos.json');
+
+    res.json(jsonData);
+}
+const nuevoparangaricutiroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/NuevoParangaricutiro.json');
+
+    res.json(jsonData);
+}
+
+const nuevourechoGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/NuevoUrecho.json');
+
+    res.json(jsonData);
+}
+const ocampoGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Ocampo.json');
+
+    res.json(jsonData);
+}
+const paracuaroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Paracuaro.json');
+
+    res.json(jsonData);
+}
+
+const patzcuaroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Patzcuaro.json');
+
+    res.json(jsonData);
+}
+
+const peribanGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Periban.json');
+
+    res.json(jsonData);
+}
+const quirogaGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Quiroga.json');
+
+    res.json(jsonData);
+}
+const salvadorescalanteGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/SalvadorEscalante.json');
+
+    res.json(jsonData);
+}
+const tacambaroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tacambaro.json');
+
+    res.json(jsonData);
+}
+const tancitaroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tancitaro.json');
+
+    res.json(jsonData);
+}
+const tangamandapioGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tangamandapio.json');
+
+    res.json(jsonData);
+}
+const tangancicuaroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tangancicuaro.json');
+
+    res.json(jsonData);
+}
+const taretanGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Taretan.json');
+
+    res.json(jsonData);
+}
+const tingambatoGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tingambato.json');
+
+    res.json(jsonData);
+}
+const tinguindinGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tinguindin.json');
+
+    res.json(jsonData);
+}
+const tlazazalcaGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tlazazalca.json');
+
+    res.json(jsonData);
+}
+const tocumboGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tocumbo.json');
+
+    res.json(jsonData);
+}
+const tuxpanGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tuxpan.json');
+
+    res.json(jsonData);
+}
+const tzintzuntzanGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Tzintzuntzan.json');
+
+    res.json(jsonData);
+}
+const zacapuGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Zacapu.json');
+
+    res.json(jsonData);
+}
+const ziracuaretiroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Ziracuaretiro.json');
+
+    res.json(jsonData);
+}
+const zitacuaroGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Zitacuaro.json');
+
+    res.json(jsonData);
+}
+
+
+const uruapanGet = (req = request, res = response) => {
+    
+    let jsonData = require('../json/Uruapan.json');
+
+    res.json(jsonData);
+}
+
+
+module.exports={
+    acuitzioGet,
+    angangueoGet,
+    apatzinganGet,
+    arioGet,
+    charapanGet,
+    charoGet,
+    churumucoGet,
+    cotijaGet,
+    erongaricuaroGet,
+    grabrielzamoraGet,
+    hidalgoGet,
+    huirambaGet,
+    irimboGet,
+    jimenezGet,
+    losreyesGet,
+    maderoGet,
+    moreliaGet,
+    morelosGet,
+    nuevoparangaricutiroGet,
+    nuevourechoGet,
+    ocampoGet,
+    paracuaroGet,
+    patzcuaroGet,
+    peribanGet,
+    quirogaGet,
+    salvadorescalanteGet,
+    tacambaroGet,
+    tancitaroGet,
+    tangamandapioGet,
+    tangancicuaroGet,
+    taretanGet,
+    tingambatoGet,
+    tinguindinGet,
+    tlazazalcaGet,
+    tocumboGet,
+    tuxpanGet,
+    tzintzuntzanGet,
+    zacapuGet,
+    ziracuaretiroGet,
+    zitacuaroGet,
+    uruapanGet
+}
